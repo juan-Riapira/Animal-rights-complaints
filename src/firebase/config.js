de4e04891702ca17c8c11939440a1209228979c5
juan-Riapira <138538939+juan-Riapira@users.noreply.github.com>
2026-06-05 07:20:18 -0500

    Update Firebase config to use environment variables

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -1,19 +1,14 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyDGjyFMRiLNfvDBVtNmyy15mb9yc0ThEq0",
-  authDomain: "cuidado-animales.firebaseapp.com",
-  projectId: "cuidado-animales",
-  storageBucket: "cuidado-animales.firebasestorage.app",
-  messagingSenderId: "25737436370",
-  appId: "1:25737436370:web:814996058247ad43d1610b"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 
-export default app
+export default app;
